87bf2957f77742075a1263f6fb8dec24a5e6db69
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2025-12-15 04:05:45 +0100

    Update pipeline

--- src/aicage/__init__.py
+++ src/aicage/__init__.py
@@ -1,2 +1,11 @@
+from importlib import metadata
+
+try:
+    from ._version import __version__
+except ImportError:
+    try:
+        __version__ = metadata.version("aicage")
+    except metadata.PackageNotFoundError:
+        __version__ = "0.0.0"
+
 __all__ = ["__version__"]
-__version__ = "0.1.0"
