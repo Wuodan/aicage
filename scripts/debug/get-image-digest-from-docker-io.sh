ecca97e3ea4c384400e68086bc59178f071077f0
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-01-12 03:07:29 +0100

    Add sample debug script to get image digest from docker.io

--- scripts/debug/get-image-digest-from-docker-io.sh
+++ scripts/debug/get-image-digest-from-docker-io.sh
@@ -10,11 +10,20 @@ TOKEN="$(curl -fsSL \
   "https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/${REPO}:pull" \
   | jq -r .token)"
 
-DIGEST="$(curl -fsSL -I \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Accept: application/vnd.oci.image.manifest.v1+json, application/vnd.docker.distribution.manifest.v2+json, application/vnd.docker.distribution.manifest.list.v2+json" \
-  "https://registry-1.docker.io/v2/library/${REPO}/manifests/${TAG}" \
+RAW="$(
+  curl -fsSL -I \
+    -H "Authorization: Bearer ${TOKEN}" \
+    -H "Accept: application/vnd.oci.image.manifest.v1+json, application/vnd.docker.distribution.manifest.v2+json, application/vnd.docker.distribution.manifest.list.v2+json" \
+    "https://registry-1.docker.io/v2/library/${REPO}/manifests/${TAG}"
+)"
+DIGEST="$(
+  echo "${RAW}" \
   | tr -d '\r' \
-  | awk -F': ' 'tolower($1)=="docker-content-digest"{print $2}')"
+  | grep -i '^docker-content-digest:' \
+  | cut -d' ' -f2
+)"
 
 echo "${IMAGE} -> ${DIGEST}"
+
+echo "Raw curl result:"
+echo "${RAW}"
